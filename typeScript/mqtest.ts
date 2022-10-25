901cf3f77e593fea7491b57271ec6bf12dbb3d6d
mqupupup <1511523340@qq.com>
2022-10-26 00:47:30 +0800

    feat:es6

--- typeScript/mqtest.ts
+++ typeScript/mqtest.ts
@@ -1,3 +1,8 @@
 window.onmousedown = function(mouseEvent: any) {
     console.log(mouseEvent.button);  //<- Now, no error is given
 };
+function createZoo(): Animal[] {
+    return [new Rhino(), new Elephant(), new Snake()];
+}
+
+Array.prototype.copyWithin(target, start = 0, end = this.length)
